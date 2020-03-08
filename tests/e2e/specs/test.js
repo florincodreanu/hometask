381d90c7d1f2e5f7c152f25cb86580c96f11f261
floorin <florin.codreanu@gmail.com>
2020-03-08 20:28:22 +0200

    keep just Jest

--- tests/e2e/specs/test.js
+++
@@ -1,8 +0,0 @@
-// https://docs.cypress.io/api/introduction/api.html
-
-describe('Test loading images', () => {
-  it('Visits the app root url', () => {
-    cy.visit('/')
-    cy.contains('h1', 'Welcome to Your Vue.js App')
-  })
-})
