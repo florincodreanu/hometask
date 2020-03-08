d963f9357fdf0ddce04ee9d6772c1e20aca92e0c
floorin <florin.codreanu@gmail.com>
2020-03-08 20:26:17 +0200

    test imagesRendered

--- tests/unit/feedback.spec.js
+++ tests/unit/feedback.spec.js
@@ -11,3 +11,14 @@ describe('totalPhotosFetched', () => {
     expect(wrapper.text()).toMatch(msg)
   })
 })
+
+
+describe('imagesRendered', () => {
+  it('renders images when photos was fetched', async () => {
+    const wrapper = shallowMount(App, {directives: {infiniteScroll} });
+    wrapper.setData({ arrayOfImages: [{"url":"https://farm4.staticflickr.com/3218/3141143944_0c209cf7fa.jpg","id":1},{"url":"https://farm3.staticflickr.com/2753/4245188018_bfa04f1f09.jpg","id":2}] });
+    await wrapper.vm.$nextTick();
+    const images = wrapper.findAll('img');
+    expect(images.length).toBe(2);
+  })
+})
